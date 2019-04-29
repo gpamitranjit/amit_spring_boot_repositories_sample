631196d47de360515734b107d7fef6596f582b88
Amit Patil <gpamitranjit@gmail.com>
2019-04-29 22:41:33 +0530

    added user defined persistence-layer

--- spring_repositories_project/src/main/java/com/amdocs/models/Todo.java
+++ spring_repositories_project/src/main/java/com/amdocs/models/Todo.java
@@ -12,7 +12,7 @@ import javax.persistence.Table;
 public class Todo {
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private String id;
 	
 	@Column(name = "name", length = 100)
