7c745ec5bfdff6b6d8b0f5a0f13da8fbe0eb29b3
Amit Patil <gpamitranjit@gmail.com>
2021-04-06 11:47:25 +0530

    class based projection in spring data jpa, note: nested projections isnt possible with class based projection although we have tweaked this limitation!!

---
+++ class-based-projection-with-spring-data/src/main/java/com/amit/app/ClassBasedProjectionWithSpringDataApplication.java
@@ -0,0 +1,13 @@
+package com.amit.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassBasedProjectionWithSpringDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClassBasedProjectionWithSpringDataApplication.class, args);
+	}
+
+}
---
+++ class-based-projection-with-spring-data/src/main/java/com/amit/app/model/managed/Person.java
@@ -0,0 +1,195 @@
+package com.amit.app.model.managed;
+
+import javax.persistence.AttributeOverride;
+import javax.persistence.AttributeOverrides;
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "persons")
+public class Person {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	Long id;
+	String firstname;
+	String lastname;
+	
+	@Embedded
+	@AttributeOverrides(
+			@AttributeOverride(name = "zipCode", column = @Column(name = "zip"))
+			)
+	Address address;
+	
+	@Embeddable
+	public static class Address {
+		String zipCode;
+		String city;
+		String street;
+		
+		public Address() {}
+		
+		public Address(String zipCode, String city, String street) {
+			super();
+			this.zipCode = zipCode;
+			this.city = city;
+			this.street = street;
+		}
+
+		@Override
+		public int hashCode() {
+			final int prime = 31;
+			int result = 1;
+			result = prime * result + ((city == null) ? 0 : city.hashCode());
+			result = prime * result + ((street == null) ? 0 : street.hashCode());
+			result = prime * result + ((zipCode == null) ? 0 : zipCode.hashCode());
+			return result;
+		}
+
+		@Override
+		public boolean equals(Object obj) {
+			if (this == obj)
+				return true;
+			if (obj == null)
+				return false;
+			if (getClass() != obj.getClass())
+				return false;
+			Address other = (Address) obj;
+			if (city == null) {
+				if (other.city != null)
+					return false;
+			} else if (!city.equals(other.city))
+				return false;
+			if (street == null) {
+				if (other.street != null)
+					return false;
+			} else if (!street.equals(other.street))
+				return false;
+			if (zipCode == null) {
+				if (other.zipCode != null)
+					return false;
+			} else if (!zipCode.equals(other.zipCode))
+				return false;
+			return true;
+		}
+
+		public String getZipCode() {
+			return zipCode;
+		}
+
+		public void setZipCode(String zipCode) {
+			this.zipCode = zipCode;
+		}
+
+		public String getCity() {
+			return city;
+		}
+
+		public void setCity(String city) {
+			this.city = city;
+		}
+
+		public String getStreet() {
+			return street;
+		}
+
+		public void setStreet(String street) {
+			this.street = street;
+		}
+	}
+
+	public Person() {}
+
+	public Person(String firstname, String lastname, Address address) {
+		this.firstname = firstname;
+		this.lastname = lastname;
+		this.address = address;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((address == null) ? 0 : address.hashCode());
+		result = prime * result + ((firstname == null) ? 0 : firstname.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((lastname == null) ? 0 : lastname.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Person other = (Person) obj;
+		if (address == null) {
+			if (other.address != null)
+				return false;
+		} else if (!address.equals(other.address))
+			return false;
+		if (firstname == null) {
+			if (other.firstname != null)
+				return false;
+		} else if (!firstname.equals(other.firstname))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (lastname == null) {
+			if (other.lastname != null)
+				return false;
+		} else if (!lastname.equals(other.lastname))
+			return false;
+		return true;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getFirstname() {
+		return firstname;
+	}
+
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+
+	public String getLastname() {
+		return lastname;
+	}
+
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+
+	public Address getAddress() {
+		return address;
+	}
+
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+
+	@Override
+	public String toString() {
+		return "Person [id=" + id + ", firstname=" + firstname + ", lastname=" + lastname + ", address=" + address
+				+ "]";
+	}
+}
