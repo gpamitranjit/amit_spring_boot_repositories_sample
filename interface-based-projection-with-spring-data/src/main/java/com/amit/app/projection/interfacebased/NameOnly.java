84616e7a00e081bd8c491b47372aa2bab1f731a0
Amit Patil <gpamitranjit@gmail.com>
2021-04-06 11:04:34 +0530

    using open projection @Value in query to database, in this case spring-data-jpa will ignore the query exection optimization because it wont be able to access the attributes of aggregate root

--- interface-based-projection-with-spring-data/src/main/java/com/amit/app/projection/interfacebased/NameOnly.java
+++ interface-based-projection-with-spring-data/src/main/java/com/amit/app/projection/interfacebased/NameOnly.java
@@ -1,7 +1,12 @@
 package com.amit.app.projection.interfacebased;
 
+import org.springframework.beans.factory.annotation.Value;
+
 public interface NameOnly {
 
 	String getFirstname();
 	String getLastname();
+	
+	@Value("#{target.firstname + ' ' + target.lastname}")
+	String getFullname();
 }
